c4d083f9df15eec7cdfa0a8f46c3480f5d1d3050
anupam858 <thewarlord858@gmail.com>
2022-06-11 17:49:05 +0530

    Changing margins in list items

--- components/MovieList.js
+++ components/MovieList.js
@@ -8,8 +8,8 @@ export default function MovieList({movies}){
             movies.map((movie) => {
                 return (
                 <div key = {movie.tconst} className={styles.movie}>
-                        <h2>{movie.primaryTitle}</h2>
-                        <h3> &nbsp;{`(${movie.startYear})`}</h3>
+                        <h2>{movie.primaryTitle} &nbsp;</h2>
+                        <h3>{`(${movie.startYear})`}</h3>
                         <p className={styles.details}>{`${movie.runtimeMinutes} mins - ${movie.genres}`}</p>
                 </div>
                 )           
